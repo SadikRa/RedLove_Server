37334fc99d057822566f6eb1719c454e802bf783
SadikRa <sadikrahman494@gmail.com>
2025-08-19 20:01:04 +0600

    updated

--- src/app/modules/donation/donation.route.ts
+++ src/app/modules/donation/donation.route.ts
@@ -5,4 +5,10 @@ const router = Router();
 
 router.post("/", donationController.createDonation);
 
+router.get("/", donationController.getDonations);
+
+router.get("/:id", donationController.getADonation);
+
+router.delete("/:id", donationController.deleteDonation);
+
 export const donationRouter = router;

9efc25e49c218410ef67daaf9b0f04a1dfb9dd21
SadikRa <sadikrahman494@gmail.com>
2025-08-19 20:00:23 +0600

    updated

--- src/app/modules/donation/donation.controller.ts
+++ src/app/modules/donation/donation.controller.ts
@@ -2,6 +2,7 @@ import httpStatus from "http-status";
 import catchAsync from "../../../shared/catchAsync";
 import sendResponse from "../../../shared/sendResponse";
 import { donationServices } from "./donation.service";
+import { Request, Response } from "express";
 
 const createDonation = catchAsync(async (req, res) => {
   const result = await donationServices.createDonation(req.body);
@@ -14,6 +15,44 @@ const createDonation = catchAsync(async (req, res) => {
   });
 });
 
+const getDonations = catchAsync(async (req: Request, res: Response) => {
+  const result = await donationServices.getDonations();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Donations retrieved successfully!",
+    data: result,
+  });
+});
+
+const getADonation = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await donationServices.getADonation(id as string);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Donation retrieved successfully!",
+    data: result,
+  });
+});
+
+const deleteDonation = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await donationServices.deleteDonation(id as string);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Donation deleted successfully!",
+    data: result,
+  });
+});
+
 export const donationController = {
   createDonation,
+  getADonation,
+  deleteDonation,
+  getDonations,
 };

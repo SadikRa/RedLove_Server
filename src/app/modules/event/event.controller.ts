8cd33d31b990d0c71206248a6636e3f8f77bcbe5
SadikRa <sadikrahman494@gmail.com>
2025-08-20 20:38:53 +0600

    event controller done

--- src/app/modules/event/event.controller.ts
+++ src/app/modules/event/event.controller.ts
@@ -4,15 +4,51 @@ import sendResponse from "../../../shared/sendResponse";
 
 const createEvent = catchAsync(async (req, res) => {
   const { id } = req.params;
-  const result = await eventServices.deleteDonation(id as string);
+  const result = await eventServices.deleteevent(id as string);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
-    message: "Donation deleted successfully!",
+    message: "event deleted successfully!",
     data: result,
   });
 });
+
+const getevents = catchAsync(async (req, res) => {
+  const result = await eventServices.getevents();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "events retrieved successfully!",
+    data: result,
+  });
+});
+
+const getAevent = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await eventServices.getAevent(id as string);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "event retrieved successfully!",
+    data: result,
+  });
+});
+
+const deleteevent = catchAsync(async (req, res) => {
+  const { id } = req.params;
+  const result = await eventServices.deleteevent(id as string);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "event deleted successfully!",
+    data: result,
+  });
+});
+
 
 export const eventController = {
   createEvent,
